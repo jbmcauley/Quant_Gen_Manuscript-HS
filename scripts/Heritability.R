8d84364a21df77c2b32449fac20fabfdf5906562
jbmcauley <john.b.mcauley@gmail.com>
2022-12-06 16:19:41 +0000

    First commit for heritability script; John

---
+++ scripts/Heritability.R
@@ -0,0 +1,15 @@
+## Heritability analysis for YAPP QC'd Dataset
+## By John McAuley
+## Dec 6th 2022
+
+
+# _________________________________________
+#  Loading Libraries
+# _________________________________________
+library(asreml)
+
+
+#__________________________________________
+#  Import Data
+#__________________________________________
+
